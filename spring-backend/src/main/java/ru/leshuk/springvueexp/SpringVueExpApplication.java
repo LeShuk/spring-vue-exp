68245173dc357169d5d6fdec24c0b6c0c9886479
leshuk <89050170077@mail.ru>
2022-08-11 17:10:31 +0400

    [0.1.0] Настройка проекта и его сборки. Функционал - нулевой

---
+++ spring-backend/src/main/java/ru/leshuk/springvueexp/SpringVueExpApplication.java
@@ -0,0 +1,13 @@
+package ru.leshuk.springvueexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueExpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVueExpApplication.class, args);
+	}
+
+}
---
+++ spring-backend/src/test/java/ru/leshuk/springvueexp/SpringVueExpApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.leshuk.springvueexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueExpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
